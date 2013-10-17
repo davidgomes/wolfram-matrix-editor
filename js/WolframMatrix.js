3543332b11fb81a7f2905023ab7243b078bc5907
David Gomes <davidrafagomes@gmail.com>
2013-10-17 18:14:32 +0100

    Initial commit.

---
+++ js/WolframMatrix.js
@@ -0,0 +1,65 @@
+/*
+ Main Controller for the simple Wolfram Alpha matrix editor
+ */
+
+function MainController($scope) {
+  /* Adds a row to the matrix editor */
+  $scope.addRow = function() {
+    var newRow = [];
+
+    for (var i = 0; i < $scope.matrix[0].length; i++) {
+      newRow.push(0);
+    }
+
+    $scope.matrix.push(newRow);
+    $scope.update();
+  };
+
+  /* Adds a column to the matrix editor */
+  $scope.addColumn = function() {
+    for (var i = 0; i < $scope.matrix.length; i++) {
+      $scope.matrix[i].push(0);
+    }
+
+    $scope.update();
+  };
+
+  /* Reads $scope.matrix and returns a string with the wolframized matrix */
+  $scope.generateMatrix = function() {
+    var output = "{";
+
+    for (var i = 0; i < $scope.matrix.length; i++) {
+      output += "{";
+
+      for (var u = 0; u < $scope.matrix[i].length; u++) {
+        output += $scope.matrix[i][u].toString();
+        
+        if (u != $scope.matrix[i].length - 1) {
+          output += ",";
+        }
+      }
+
+      if (i != $scope.matrix.length - 1) {
+        output += "},";
+      } else {
+        output += "}";
+      }
+    }
+
+    return output + "}";
+  };
+
+  /* Updates the value in the output text box */
+  $scope.update = function() {
+    $scope.output = $scope.generateMatrix();
+  };
+
+  /* Searches the wolframized string on Wolfram Alpha */
+  $scope.wolframSearch = function() {
+    var win = window.open("http://www.wolframalpha.com/input/?i=" + $scope.output, '_blank');
+  };
+  
+  /* Initializes the matrix with a bunch of zeros */
+  $scope.matrix = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
+  $scope.output = $scope.generateMatrix($scope.matrix);
+}
