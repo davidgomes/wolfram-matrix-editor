766e28a10d639d98d140d475dd7e5e254b2a5572
David Gomes <davidrafagomes@gmail.com>
2013-10-30 23:08:34 +0000

    Implemented removeRow and removeColumn.

--- js/WolframMatrix.js
+++ js/WolframMatrix.js
@@ -15,10 +15,25 @@ function MainController($scope) {
     $scope.update();
   };
 
+  /* Remove a row from the matrix editor */
+  $scope.removeRow = function() {
+    $scope.matrix.pop();
+    $scope.update();
+  };
+
   /* Adds a column to the matrix editor */
   $scope.addColumn = function() {
     for (var i = 0; i < $scope.matrix.length; i++) {
       $scope.matrix[i].push(0);
+    }
+
+    $scope.update();
+  };
+
+  /* Removes a column from the matrix editor */
+  $scope.removeColumn = function() {
+    for (var i = 0; i < $scope.matrix.length; i++) {
+      $scope.matrix[i].pop();
     }
 
     $scope.update();
@@ -33,7 +48,7 @@ function MainController($scope) {
 
       for (var u = 0; u < $scope.matrix[i].length; u++) {
         output += $scope.matrix[i][u].toString();
-        
+
         if (u != $scope.matrix[i].length - 1) {
           output += ",";
         }
@@ -58,7 +73,7 @@ function MainController($scope) {
   $scope.wolframSearch = function() {
     var win = window.open("http://www.wolframalpha.com/input/?i=" + $scope.output, '_blank');
   };
-  
+
   /* Initializes the matrix with a bunch of zeros */
   $scope.matrix = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
   $scope.output = $scope.generateMatrix($scope.matrix);
